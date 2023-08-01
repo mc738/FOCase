5e040f624c4590f808021e407729a96e99060fd0
Max Clifford <me@mclifford.dev>
2023-08-02 00:02:47 +0100

    Work on Resources.fs

---
+++ FOCase/Store/V1/Resources.fs
@@ -0,0 +1,33 @@
+﻿namespace FOCase.Store.V1
+
+[<RequireQualifiedAccess>]
+module Resources =
+    
+    open Freql.Sqlite
+    open FOCase.Core
+    open FOCase.Store.V1.Persistence
+
+    // *** General ***
+
+    let add (ctx: SqliteContext) (id: IdType option) (name: string) =
+        ({ Id = getId id
+           Name = name
+           CreatedOn = getTimestamp ()
+           Active = true }
+        : Parameters.NewResource)
+        |> Operations.insertResource ctx
+
+    let get (ctx: SqliteContext) (id: string) =
+        Operations.selectResourceRecord ctx [ "WHERE id = @0" ] [ id ]
+        
+    let getAll (ctx:SqliteContext) =
+        Operations.selectResourceRecords ctx [] []
+        
+    let getAllActive (ctx: SqliteContext) =
+        Operations.selectResourceRecords ctx [ "WHERE active = TRUE" ] []
+    
+    let activate (ctx: SqliteContext) (id: string) =
+        ctx.ExecuteVerbatimNonQueryAnon("UPDATE resources SET active = TRUE WHERE id = @0", [ id ])
+
+    let deactivate (ctx: SqliteContext) (id: string) =
+        ctx.ExecuteVerbatimNonQueryAnon("UPDATE resources SET active = FALSE WHERE id = @0", [ id ])
