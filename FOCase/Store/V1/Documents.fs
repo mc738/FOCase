c75621e364cd6fe84b646c47b169f8b89ea7d8ce
Max Clifford <me@mclifford.dev>
2023-08-03 20:03:05 +0100

    Added Documents.fs notes

--- FOCase/Store/V1/Documents.fs
+++ FOCase/Store/V1/Documents.fs
@@ -120,3 +120,90 @@ module Documents =
             "UPDATE document_tags SET active = FALSE WHERE document_id = @0 AND tag = @1",
             [ documentId; tag ]
         )
+        
+    // *** Notes ***
+
+    let getNote (ctx: SqliteContext) (noteId: string) =
+        Operations.selectDocumentNoteRecord ctx [ "WHERE note_id = @0" ] [ noteId ]
+
+    let getAllActiveNotes (ctx: SqliteContext) (documentId: string) =
+        Operations.selectDocumentNoteRecord ctx [ "WHERE document_id = @0 AND active = TRUE;" ] [ documentId ]
+
+    let getAllNotes (ctx: SqliteContext) (documentId: string) =
+        Operations.selectDocumentNoteRecord ctx [ "WHERE document_id = @0" ] [ documentId ]
+
+    let getLatestNoteVersion (ctx: SqliteContext) (noteId: string) =
+        Operations.selectDocumentNoteVersionRecord
+            ctx
+            [ "WHERE document_note_id = @0 ORDER BY version DESC LIMIT 1" ]
+            [ noteId ]
+
+    let getLatestActiveNoteVersion (ctx: SqliteContext) (noteId: string) =
+        Operations.selectDocumentNoteVersionRecord
+            ctx
+            [ "WHERE document_note_id = @0 AND active = TRUE ORDER BY version DESC LIMIT 1" ]
+            [ noteId ]
+
+    let activateNote (ctx: SqliteContext) (noteId: string) =
+        ctx.ExecuteVerbatimNonQueryAnon("UPDATE document_notes SET active = TRUE WHERE id = @0", [ noteId ])
+
+    let deactivateNote (ctx: SqliteContext) (noteId: string) =
+        ctx.ExecuteVerbatimNonQueryAnon("UPDATE document_notes SET active = FALSE WHERE id = @0", [ noteId ])
+
+    let activateNoteVersion (ctx: SqliteContext) (noteVersionId: string) =
+        ctx.ExecuteVerbatimNonQueryAnon("UPDATE document_note_versions SET active = TRUE WHERE id = @0", [ noteVersionId ])
+
+    let deactivateNoteVersion (ctx: SqliteContext) (noteVersionId: string) =
+        ctx.ExecuteVerbatimNonQueryAnon("UPDATE document_note_versions SET active = FALSE WHERE id = @0", [ noteVersionId ])
+
+
+    let addNote (ctx: SqliteContext) (id: IdType option) (documentId: string) =
+        ({ Id = getId id
+           DocumentId = documentId
+           CreatedOn = getTimestamp ()
+           Active = true }
+        : Parameters.NewDocumentNote)
+        |> Operations.insertDocumentNote ctx
+
+    let addNoteVersion
+        (ctx: SqliteContext)
+        (id: IdType option)
+        (noteId: string)
+        (version: int)
+        (title: string)
+        (note: string)
+        =
+        use ms = new MemoryStream(note.ToUtf8Bytes())
+        let hash = ms.GetSHA256Hash()
+
+        ({ Id = getId id
+           DocumentNoteId = noteId
+           Version = version
+           Title = title
+           Note = BlobField.FromStream ms
+           Hash = hash
+           CreatedOn = getTimestamp ()
+           Active = true }
+        : Parameters.NewDocumentNoteVersion)
+        |> Operations.insertDocumentNoteVersion ctx
+
+    let tryAddLatestNoteVersion
+        (ctx: SqliteContext)
+        (id: IdType option)
+        (noteId: string)
+        (title: string)
+        (note: string)
+        =
+        match getLatestNoteVersion ctx noteId with
+        | Some lnv -> addNoteVersion ctx id noteId (lnv.Version + 1) title note |> Ok
+        | None -> Error $"Document note `{noteId}` does not exist"
+
+    let addNewNote (ctx: SqliteContext) (documentId: string) (title: string) (note: string) =
+        let id = IdType.Create()
+        addNote ctx (Some id) documentId
+        addNoteVersion ctx None (id.GetId()) 1 title note
+
+    let tryAddNewNote (ctx: SqliteContext) (documentId: string) (title: string) (note: string) =
+        match get ctx documentId with
+        | Some _ -> addNewNote ctx documentId title note |> Ok
+        | None -> Error $"Document `{documentId}` does not exist"
