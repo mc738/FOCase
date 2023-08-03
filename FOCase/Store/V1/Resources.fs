d990cac6d64896af6f9b55be565d31ea875f0df6
Max Clifford <me@mclifford.dev>
2023-08-03 19:27:16 +0100

    Added resource metadata

--- FOCase/Store/V1/Resources.fs
+++ FOCase/Store/V1/Resources.fs
@@ -31,3 +31,51 @@ module Resources =
 
     let deactivate (ctx: SqliteContext) (id: string) =
         ctx.ExecuteVerbatimNonQueryAnon("UPDATE resources SET active = FALSE WHERE id = @0", [ id ])
+
+    // *** Metadata ***
+    
+    let getMetadataValue (ctx: SqliteContext) (resourceId: string) (key: string) =
+        Operations.selectResourceMetadataItemRecord ctx [ "WHERE resource_id = @0 AND item_key = @1" ] [ resourceId; key ]
+
+    let addMetadataValue (ctx: SqliteContext) (resourceId: string) (key: string) (value: string) =
+        ({ ResourceId = resourceId
+           ItemKey = key
+           ItemValue = value
+           CreatedOn = getTimestamp ()
+           Active = true }
+        : Parameters.NewResourceMetadataItem)
+        |> Operations.insertResourceMetadataItem ctx
+
+    let tryAddMetadataValue (ctx: SqliteContext) (resourceId: string) (key: string) (value: string) =
+        match getMetadataValue ctx resourceId key with
+        | Some _ -> Error $"Metadata value `{key}` already exists for resource `{resourceId}`"
+        | None -> addMetadataValue ctx resourceId key value |> Ok
+
+    let updateMetadataValue (ctx: SqliteContext) (resourceId: string) (key: string) (value: string) =
+        ctx.ExecuteVerbatimNonQueryAnon(
+            "UPDATE resource_metadata SET item_value = @0 WHERE resource_id = @1 AND item_key = @2",
+            [ value; resourceId; key ]
+        )
+        |> ignore
+
+    let tryUpdateMetadataValue (ctx: SqliteContext) (resourceId: string) (key: string) (value: string) =
+        match getMetadataValue ctx resourceId key with
+        | Some _ -> updateMetadataValue ctx resourceId key value |> Ok
+        | None -> Error $"Metadata value `{key}` does not exist for resource `{resourceId}`"
+
+    let addOrUpdateMetadataValue (ctx: SqliteContext) (resourceId: string) (key: string) (value: string) =
+        match getMetadataValue ctx resourceId key with
+        | Some _ -> updateMetadataValue ctx resourceId key value
+        | None -> addMetadataValue ctx resourceId key value
+
+    let activateMetadataItem (ctx: SqliteContext) (resourceId: string) (key: string) =
+        ctx.ExecuteVerbatimNonQueryAnon(
+            "UPDATE resource_metadata SET active = TRUE WHERE resource_id = @0 AND item_key = @1",
+            [ resourceId; key ]
+        )
+
+    let deactivateMetadataItem (ctx: SqliteContext) (resourceId: string) (key: string) =
+        ctx.ExecuteVerbatimNonQueryAnon(
+            "UPDATE resource_metadata SET active = FALSE WHERE resource_id = @0 AND item_key = @1",
+            [ resourceId; key ]
+        )
