4f4a323190023d979de1c7fe9af6ca86849315e3
Max Clifford <me@mclifford.dev>
2023-08-07 18:38:22 +0100

    Added NodeResources.fs notes

--- FOCase/Store/V1/NodeResources.fs
+++ FOCase/Store/V1/NodeResources.fs
@@ -60,3 +60,90 @@ module NodeResources =
             [ nodeResourceId; key ]
         )
 
+
+    // *** Notes ***
+
+    let getNote (ctx: SqliteContext) (noteId: string) =
+        Operations.selectNodeResourceNoteRecord ctx [ "WHERE note_id = @0" ] [ noteId ]
+
+    let getAllActiveNotes (ctx: SqliteContext) (nodeResourceId: string) =
+        Operations.selectNodeResourceNoteRecord ctx [ "WHERE node_resource_id = @0 AND active = TRUE;" ] [ nodeResourceId ]
+
+    let getAllNotes (ctx: SqliteContext) (nodeResourceId: string) =
+        Operations.selectNodeResourceNoteRecord ctx [ "WHERE node_resource_id = @0" ] [ nodeResourceId ]
+
+    let getLatestNoteVersion (ctx: SqliteContext) (noteId: string) =
+        Operations.selectNodeResourceNoteVersionRecord
+            ctx
+            [ "WHERE node_resource_note_id = @0 ORDER BY version DESC LIMIT 1" ]
+            [ noteId ]
+
+    let getLatestActiveNoteVersion (ctx: SqliteContext) (noteId: string) =
+        Operations.selectNodeResourceNoteVersionRecord
+            ctx
+            [ "WHERE node_resource_note_id = @0 AND active = TRUE ORDER BY version DESC LIMIT 1" ]
+            [ noteId ]
+
+    let activateNote (ctx: SqliteContext) (noteId: string) =
+        ctx.ExecuteVerbatimNonQueryAnon("UPDATE node_resource_notes SET active = TRUE WHERE id = @0", [ noteId ])
+
+    let deactivateNote (ctx: SqliteContext) (noteId: string) =
+        ctx.ExecuteVerbatimNonQueryAnon("UPDATE node_resource_notes SET active = FALSE WHERE id = @0", [ noteId ])
+
+    let activateNoteVersion (ctx: SqliteContext) (noteVersionId: string) =
+        ctx.ExecuteVerbatimNonQueryAnon("UPDATE node_resource_note_versions SET active = TRUE WHERE id = @0", [ noteVersionId ])
+
+    let deactivateNoteVersion (ctx: SqliteContext) (noteVersionId: string) =
+        ctx.ExecuteVerbatimNonQueryAnon("UPDATE node_resource_note_versions SET active = FALSE WHERE id = @0", [ noteVersionId ])
+
+
+    let addNote (ctx: SqliteContext) (id: IdType option) (nodeResourceId: string) =
+        ({ Id = getId id
+           NodeResourceId = nodeResourceId
+           CreatedOn = getTimestamp ()
+           Active = true }
+        : Parameters.NewNodeResourceNote)
+        |> Operations.insertNodeResourceNote ctx
+
+    let addNoteVersion
+        (ctx: SqliteContext)
+        (id: IdType option)
+        (noteId: string)
+        (version: int)
+        (title: string)
+        (note: string)
+        =
+        use ms = new MemoryStream(note.ToUtf8Bytes())
+        let hash = ms.GetSHA256Hash()
+
+        ({ Id = getId id
+           NodeResourceNoteId = noteId
+           Version = version
+           Title = title
+           Note = BlobField.FromStream ms
+           Hash = hash
+           CreatedOn = getTimestamp ()
+           Active = true }
+        : Parameters.NewNodeResourceNoteVersion)
+        |> Operations.insertNodeResourceNoteVersion ctx
+
+    let tryAddLatestNoteVersion
+        (ctx: SqliteContext)
+        (id: IdType option)
+        (noteId: string)
+        (title: string)
+        (note: string)
+        =
+        match getLatestNoteVersion ctx noteId with
+        | Some lnv -> addNoteVersion ctx id noteId (lnv.Version + 1) title note |> Ok
+        | None -> Error $"Node resource note `{noteId}` does not exist"
+
+    let addNewNote (ctx: SqliteContext) (nodeResourceId: string) (title: string) (note: string) =
+        let id = IdType.Create()
+        addNote ctx (Some id) nodeResourceId
+        addNoteVersion ctx None (id.GetId()) 1 title note
+
+    let tryAddNewNote (ctx: SqliteContext) (nodeResourceId: string) (title: string) (note: string) =
+        match get ctx node_resourceId with
+        | Some _ -> addNewNote ctx nodeResourceId title note |> Ok
+        | None -> Error $"Node resource `{nodeResourceId}` does not exist"
