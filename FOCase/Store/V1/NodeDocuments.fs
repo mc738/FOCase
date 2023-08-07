f97b5a03787b5792ea0d779a7e4437708492fcf7
Max Clifford <me@mclifford.dev>
2023-08-07 18:35:16 +0100

    Added NodeDocuments.fs notes

--- FOCase/Store/V1/NodeDocuments.fs
+++ FOCase/Store/V1/NodeDocuments.fs
@@ -60,5 +60,92 @@ module NodeDocuments =
     let deactivateMetadataItem (ctx: SqliteContext) (nodeDocumentId: string) (key: string) =
         ctx.ExecuteVerbatimNonQueryAnon(
             "UPDATE node_document_metadata SET active = FALSE WHERE node_document_id = @0 AND item_key = @1",
-            [ node_documentId; key ]
+            [ nodeDocumentId; key ]
         )
+        
+    
+    // *** Notes ***
+
+    let getNote (ctx: SqliteContext) (noteId: string) =
+        Operations.selectNodeDocumentNoteRecord ctx [ "WHERE note_id = @0" ] [ noteId ]
+
+    let getAllActiveNotes (ctx: SqliteContext) (nodeDocumentId: string) =
+        Operations.selectNodeDocumentNoteRecord ctx [ "WHERE node_document_id = @0 AND active = TRUE;" ] [ nodeDocumentId ]
+
+    let getAllNotes (ctx: SqliteContext) (nodeDocumentId: string) =
+        Operations.selectNodeDocumentNoteRecord ctx [ "WHERE node_document_id = @0" ] [ nodeDocumentId ]
+
+    let getLatestNoteVersion (ctx: SqliteContext) (noteId: string) =
+        Operations.selectNodeDocumentNoteVersionRecord
+            ctx
+            [ "WHERE node_document_note_id = @0 ORDER BY version DESC LIMIT 1" ]
+            [ noteId ]
+
+    let getLatestActiveNoteVersion (ctx: SqliteContext) (noteId: string) =
+        Operations.selectNodeDocumentNoteVersionRecord
+            ctx
+            [ "WHERE node_document_note_id = @0 AND active = TRUE ORDER BY version DESC LIMIT 1" ]
+            [ noteId ]
+
+    let activateNote (ctx: SqliteContext) (noteId: string) =
+        ctx.ExecuteVerbatimNonQueryAnon("UPDATE node_document_notes SET active = TRUE WHERE id = @0", [ noteId ])
+
+    let deactivateNote (ctx: SqliteContext) (noteId: string) =
+        ctx.ExecuteVerbatimNonQueryAnon("UPDATE node_document_notes SET active = FALSE WHERE id = @0", [ noteId ])
+
+    let activateNoteVersion (ctx: SqliteContext) (noteVersionId: string) =
+        ctx.ExecuteVerbatimNonQueryAnon("UPDATE node_document_note_versions SET active = TRUE WHERE id = @0", [ noteVersionId ])
+
+    let deactivateNoteVersion (ctx: SqliteContext) (noteVersionId: string) =
+        ctx.ExecuteVerbatimNonQueryAnon("UPDATE node_document_note_versions SET active = FALSE WHERE id = @0", [ noteVersionId ])
+
+    let addNote (ctx: SqliteContext) (id: IdType option) (nodeDocumentId: string) =
+        ({ Id = getId id
+           NodeDocumentId = nodeDocumentId
+           CreatedOn = getTimestamp ()
+           Active = true }
+        : Parameters.NewNodeDocumentNote)
+        |> Operations.insertNodeDocumentNote ctx
+
+    let addNoteVersion
+        (ctx: SqliteContext)
+        (id: IdType option)
+        (noteId: string)
+        (version: int)
+        (title: string)
+        (note: string)
+        =
+        use ms = new MemoryStream(note.ToUtf8Bytes())
+        let hash = ms.GetSHA256Hash()
+
+        ({ Id = getId id
+           NodeDocumentNoteId = noteId
+           Version = version
+           Title = title
+           Note = BlobField.FromStream ms
+           Hash = hash
+           CreatedOn = getTimestamp ()
+           Active = true }
+        : Parameters.NewNodeDocumentNoteVersion)
+        |> Operations.insertNodeDocumentNoteVersion ctx
+
+    let tryAddLatestNoteVersion
+        (ctx: SqliteContext)
+        (id: IdType option)
+        (noteId: string)
+        (title: string)
+        (note: string)
+        =
+        match getLatestNoteVersion ctx noteId with
+        | Some lnv -> addNoteVersion ctx id noteId (lnv.Version + 1) title note |> Ok
+        | None -> Error $"NodeDocument note `{noteId}` does not exist"
+
+    let addNewNote (ctx: SqliteContext) (nodeDocumentId: string) (title: string) (note: string) =
+        let id = IdType.Create()
+        addNote ctx (Some id) nodeDocumentId
+        addNoteVersion ctx None (id.GetId()) 1 title note
+
+    let tryAddNewNote (ctx: SqliteContext) (nodeDocumentId: string) (title: string) (note: string) =
+        match get ctx node_documentId with
+        | Some _ -> addNewNote ctx nodeDocumentId title note |> Ok
+        | None -> Error $"Node document `{nodeDocumentId}` does not exist"
