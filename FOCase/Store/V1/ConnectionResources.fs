9e8c2fa6e1e44163505a84cbe0c072afb5f16e25
Max Clifford <me@mclifford.dev>
2023-08-08 20:19:12 +0100

    Added ConnectionResources.fs

---
+++ FOCase/Store/V1/ConnectionResources.fs
@@ -0,0 +1,17 @@
+﻿namespace FOCase.Store.V1
+
+[<RequireQualifiedAccess>]
+module ConnectionResources =
+    
+    open System.IO
+    open Freql.Core.Common.Types
+    open FsToolbox.Extensions.Strings
+    open FsToolbox.Extensions.Streams
+    open Freql.Sqlite
+    open FOCase.Core
+    open FOCase.Store.V1.Persistence
+        
+    
+    
+    ()
+
