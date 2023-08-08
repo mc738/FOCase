925e8f364844d109121d879c28b51c49ff053339
Max Clifford <me@mclifford.dev>
2023-08-08 20:14:42 +0100

    Added ConnectionDocuments.fs

---
+++ FOCase/Store/V1/ConnectionDocuments.fs
@@ -0,0 +1,16 @@
+﻿namespace FOCase.Store.V1
+
+[<RequireQualifiedAccess>]
+module ConnectionDocuments =
+    
+    open System.IO
+    open Freql.Core.Common.Types
+    open FsToolbox.Extensions.Strings
+    open FsToolbox.Extensions.Streams
+    open Freql.Sqlite
+    open FOCase.Core
+    open FOCase.Store.V1.Persistence
+    
+    
+    ()
+
