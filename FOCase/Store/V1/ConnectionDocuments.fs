334a957ef79acd0028c346be24ea4e0030958e0a
Max Clifford <me@mclifford.dev>
2023-08-08 20:17:22 +0100

    Added ConnectionDocuments.fs notes

--- FOCase/Store/V1/ConnectionDocuments.fs
+++ FOCase/Store/V1/ConnectionDocuments.fs
@@ -59,6 +59,91 @@ module ConnectionDocuments =
             [ connectionDocumentId; key ]
         )
     
+
     
-    ()
+    // *** Notes ***
+
+    let getNote (ctx: SqliteContext) (noteId: string) =
+        Operations.selectConnectionDocumentNoteRecord ctx [ "WHERE note_id = @0" ] [ noteId ]
+
+    let getAllActiveNotes (ctx: SqliteContext) (connectionDocumentId: string) =
+        Operations.selectConnectionDocumentNoteRecord ctx [ "WHERE connection_document_id = @0 AND active = TRUE;" ] [ connectionDocumentId ]
+
+    let getAllNotes (ctx: SqliteContext) (connectionDocumentId: string) =
+        Operations.selectConnectionDocumentNoteRecord ctx [ "WHERE connection_document_id = @0" ] [ connectionDocumentId ]
+
+    let getLatestNoteVersion (ctx: SqliteContext) (noteId: string) =
+        Operations.selectConnectionDocumentNoteVersionRecord
+            ctx
+            [ "WHERE connection_document_note_id = @0 ORDER BY version DESC LIMIT 1" ]
+            [ noteId ]
+
+    let getLatestActiveNoteVersion (ctx: SqliteContext) (noteId: string) =
+        Operations.selectConnectionDocumentNoteVersionRecord
+            ctx
+            [ "WHERE connection_document_note_id = @0 AND active = TRUE ORDER BY version DESC LIMIT 1" ]
+            [ noteId ]
+
+    let activateNote (ctx: SqliteContext) (noteId: string) =
+        ctx.ExecuteVerbatimNonQueryAnon("UPDATE connection_document_notes SET active = TRUE WHERE id = @0", [ noteId ])
+
+    let deactivateNote (ctx: SqliteContext) (noteId: string) =
+        ctx.ExecuteVerbatimNonQueryAnon("UPDATE connection_document_notes SET active = FALSE WHERE id = @0", [ noteId ])
+
+    let activateNoteVersion (ctx: SqliteContext) (noteVersionId: string) =
+        ctx.ExecuteVerbatimNonQueryAnon("UPDATE connection_document_note_versions SET active = TRUE WHERE id = @0", [ noteVersionId ])
+
+    let deactivateNoteVersion (ctx: SqliteContext) (noteVersionId: string) =
+        ctx.ExecuteVerbatimNonQueryAnon("UPDATE connection_document_note_versions SET active = FALSE WHERE id = @0", [ noteVersionId ])
+
+    let addNote (ctx: SqliteContext) (id: IdType option) (connectionDocumentId: string) =
+        ({ Id = getId id
+           ConnectionDocumentId = connectionDocumentId
+           CreatedOn = getTimestamp ()
+           Active = true }
+        : Parameters.NewConnectionDocumentNote)
+        |> Operations.insertConnectionDocumentNote ctx
+
+    let addNoteVersion
+        (ctx: SqliteContext)
+        (id: IdType option)
+        (noteId: string)
+        (version: int)
+        (title: string)
+        (note: string)
+        =
+        use ms = new MemoryStream(note.ToUtf8Bytes())
+        let hash = ms.GetSHA256Hash()
+
+        ({ Id = getId id
+           ConnectionDocumentNoteId = noteId
+           Version = version
+           Title = title
+           Note = BlobField.FromStream ms
+           Hash = hash
+           CreatedOn = getTimestamp ()
+           Active = true }
+        : Parameters.NewConnectionDocumentNoteVersion)
+        |> Operations.insertConnectionDocumentNoteVersion ctx
+
+    let tryAddLatestNoteVersion
+        (ctx: SqliteContext)
+        (id: IdType option)
+        (noteId: string)
+        (title: string)
+        (note: string)
+        =
+        match getLatestNoteVersion ctx noteId with
+        | Some lnv -> addNoteVersion ctx id noteId (lnv.Version + 1) title note |> Ok
+        | None -> Error $"ConnectionDocument note `{noteId}` does not exist"
+
+    let addNewNote (ctx: SqliteContext) (connectionDocumentId: string) (title: string) (note: string) =
+        let id = IdType.Create()
+        addNote ctx (Some id) connectionDocumentId
+        addNoteVersion ctx None (id.GetId()) 1 title note
+
+    let tryAddNewNote (ctx: SqliteContext) (connectionDocumentId: string) (title: string) (note: string) =
+        match get ctx connectionDocumentId with
+        | Some _ -> addNewNote ctx connectionDocumentId title note |> Ok
+        | None -> Error $"Node document `{connectionDocumentId}` does not exist"
 
