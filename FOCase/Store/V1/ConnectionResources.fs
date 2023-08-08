5d4d148b968159dc47fa586747e8d6fab3fa2927
Max Clifford <me@mclifford.dev>
2023-08-08 20:21:56 +0100

    Added ConnectionResources.fs notes

--- FOCase/Store/V1/ConnectionResources.fs
+++ FOCase/Store/V1/ConnectionResources.fs
@@ -60,6 +60,89 @@ module ConnectionResources =
             [ connectionResourceId; key ]
         )
 
-    
-    ()
+    // *** Notes ***
 
+    let getNote (ctx: SqliteContext) (noteId: string) =
+        Operations.selectConnectionResourceNoteRecord ctx [ "WHERE note_id = @0" ] [ noteId ]
+
+    let getAllActiveNotes (ctx: SqliteContext) (connectionResourceId: string) =
+        Operations.selectConnectionResourceNoteRecord ctx [ "WHERE connection_resource_id = @0 AND active = TRUE;" ] [ connectionResourceId ]
+
+    let getAllNotes (ctx: SqliteContext) (connectionResourceId: string) =
+        Operations.selectConnectionResourceNoteRecord ctx [ "WHERE connection_resource_id = @0" ] [ connectionResourceId ]
+
+    let getLatestNoteVersion (ctx: SqliteContext) (noteId: string) =
+        Operations.selectConnectionResourceNoteVersionRecord
+            ctx
+            [ "WHERE connection_resource_note_id = @0 ORDER BY version DESC LIMIT 1" ]
+            [ noteId ]
+
+    let getLatestActiveNoteVersion (ctx: SqliteContext) (noteId: string) =
+        Operations.selectConnectionResourceNoteVersionRecord
+            ctx
+            [ "WHERE connection_resource_note_id = @0 AND active = TRUE ORDER BY version DESC LIMIT 1" ]
+            [ noteId ]
+
+    let activateNote (ctx: SqliteContext) (noteId: string) =
+        ctx.ExecuteVerbatimNonQueryAnon("UPDATE connection_resource_notes SET active = TRUE WHERE id = @0", [ noteId ])
+
+    let deactivateNote (ctx: SqliteContext) (noteId: string) =
+        ctx.ExecuteVerbatimNonQueryAnon("UPDATE connection_resource_notes SET active = FALSE WHERE id = @0", [ noteId ])
+
+    let activateNoteVersion (ctx: SqliteContext) (noteVersionId: string) =
+        ctx.ExecuteVerbatimNonQueryAnon("UPDATE connection_resource_note_versions SET active = TRUE WHERE id = @0", [ noteVersionId ])
+
+    let deactivateNoteVersion (ctx: SqliteContext) (noteVersionId: string) =
+        ctx.ExecuteVerbatimNonQueryAnon("UPDATE connection_resource_note_versions SET active = FALSE WHERE id = @0", [ noteVersionId ])
+
+
+    let addNote (ctx: SqliteContext) (id: IdType option) (connectionResourceId: string) =
+        ({ Id = getId id
+           ConnectionResourceId = connectionResourceId
+           CreatedOn = getTimestamp ()
+           Active = true }
+        : Parameters.NewConnectionResourceNote)
+        |> Operations.insertConnectionResourceNote ctx
+
+    let addNoteVersion
+        (ctx: SqliteContext)
+        (id: IdType option)
+        (noteId: string)
+        (version: int)
+        (title: string)
+        (note: string)
+        =
+        use ms = new MemoryStream(note.ToUtf8Bytes())
+        let hash = ms.GetSHA256Hash()
+
+        ({ Id = getId id
+           ConnectionResourceNoteId = noteId
+           Version = version
+           Title = title
+           Note = BlobField.FromStream ms
+           Hash = hash
+           CreatedOn = getTimestamp ()
+           Active = true }
+        : Parameters.NewConnectionResourceNoteVersion)
+        |> Operations.insertConnectionResourceNoteVersion ctx
+
+    let tryAddLatestNoteVersion
+        (ctx: SqliteContext)
+        (id: IdType option)
+        (noteId: string)
+        (title: string)
+        (note: string)
+        =
+        match getLatestNoteVersion ctx noteId with
+        | Some lnv -> addNoteVersion ctx id noteId (lnv.Version + 1) title note |> Ok
+        | None -> Error $"Node resource note `{noteId}` does not exist"
+
+    let addNewNote (ctx: SqliteContext) (connectionResourceId: string) (title: string) (note: string) =
+        let id = IdType.Create()
+        addNote ctx (Some id) connectionResourceId
+        addNoteVersion ctx None (id.GetId()) 1 title note
+
+    let tryAddNewNote (ctx: SqliteContext) (connectionResourceId: string) (title: string) (note: string) =
+        match get ctx connectionResourceId with
+        | Some _ -> addNewNote ctx connectionResourceId title note |> Ok
+        | None -> Error $"Node resource `{connectionResourceId}` does not exist"
