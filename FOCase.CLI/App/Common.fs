b60e03a6798dd04abed8270183df28718e82538c
Max Clifford <me@mclifford.dev>
2023-08-10 20:11:37 +0100

    Added optionPrompt

--- FOCase.CLI/App/Common.fs
+++ FOCase.CLI/App/Common.fs
@@ -3,6 +3,7 @@
 [<AutoOpen>]
 module Common =
     
+    open System
     
     type UserOption =
         { Name: string
@@ -10,5 +11,51 @@ module Common =
           AcceptedValues: string list }
 
     
-    ()
+    
+    let printLines (lines: string list) = lines |> List.iter Console.WriteLine
+
+    let optionPrompt (prompt: string) (marker: string option) (options: UserOption list) =
+        
+        let printOptions _ =
+            options
+            |> List.iteri (fun i o ->
+                let acceptedValues = o.AcceptedValues |> String.concat " / "
+
+                printfn $"{i + 1}. {o.Name} ({acceptedValues})"
+                o.Details |> Option.iter (fun d -> printfn $"\t{d}"))
+
+
+        let rec handler _ =
+            Console.WriteLine prompt
+
+            printOptions ()
+
+            match marker with
+            | Some m -> printf $"{marker} > "
+            | None -> printf "> "
+            
+            let input = Console.ReadLine()
+
+            match input with
+            | "clr"
+            | "clear" ->
+                Console.Clear()
+                handler ()
+            | _ ->
+                
+                let selectedOption =
+                    options
+                    |> List.tryFind (fun o ->
+                        o.AcceptedValues
+                        |> List.exists (fun av -> System.String.Equals(input, av, StringComparison.OrdinalIgnoreCase)))
+
+                match selectedOption with
+                | Some so -> so
+                | None ->
+                    printfn $"Unknown option: {input}"
+                    printfn ""
+                    handler ()
+
+        handler ()
+    
 
