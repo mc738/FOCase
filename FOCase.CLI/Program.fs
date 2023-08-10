69fbf4c30982a0f9355ee5d2bd61475d116fdca9
Max Clifford <me@mclifford.dev>
2023-08-10 18:32:36 +0100

    Added banner

--- FOCase.CLI/Program.fs
+++ FOCase.CLI/Program.fs
@@ -1,7 +1,51 @@
-﻿
-let header =
-    ()
+﻿open System
+
+module Utils =
+    
+    
+    type Option =
+        {
+            Name: string
+            Details: string option
+            AcceptedValues: string
+        }
+    
+    let printLines (lines: string list) = lines |> List.iter Console.WriteLine
+
+    let optionPrompt (prompt: string) (options: Option list) =
+        Console.WriteLine prompt
+        
+        options |> List.mapi (fun i o -> printfn $"{i + 1}. {}" )
+        
+        
+        
+        
+        
+        ()
+
+let banner =
+    [
+        "▄████  ████▄ ▄█▄    ██      ▄▄▄▄▄   ▄███▄   "
+        "█▀   ▀ █   █ █▀ ▀▄  █ █    █     ▀▄ █▀   ▀  "
+        "█▀▀    █   █ █   ▀  █▄▄█ ▄  ▀▀▀▀▄   ██▄▄    "
+        "█      ▀████ █▄  ▄▀ █  █  ▀▄▄▄▄▀    █▄   ▄▀ "
+        " █           ▀███▀     █            ▀███▀   "
+        "  ▀                   █                     "
+        "                     ▀                      "
+        ""
+        "Version: 0.1 "
+    ]
+
+let initialOptions =
+    [
+        "Choose an option:"
+        "1. New case (new/n/1)"
+        "2. Load case (load/l/2)"
+    ]
 
 
-// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+banner |> List.iter Console.WriteLine
+initialOptions |> List.iter Console.WriteLine
+
+Console.ReadLine ()
+
