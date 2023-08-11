c2e1cd89bc7a07663a81c5fdc581b1011b1df3bc
Max Clifford <me@mclifford.dev>
2023-08-11 19:01:57 +0100

    Added Impl.fs

---
+++ FOCase.CLI/App/Impl.fs
@@ -0,0 +1,33 @@
+﻿namespace FOCase.CLI.App
+
+[<AutoOpen>]
+module Impl =
+
+    [<AutoOpen>]
+    module private Internal =
+
+        let initialOptions =
+            ({ Prompt = ""
+               Marker = None
+               Items =
+                 [ { Name = "New case"
+                     Details = None
+                     AcceptedValues = [ "new"; "n" ] }
+                   { Name = "New case"
+                     Details = None
+                     AcceptedValues = [ "load"; "l" ] } ] }
+            : OptionPrompt)
+
+        ()
+
+    let run _ =
+
+        printBanner ()
+        let mode = optionPrompt initialOptions
+
+
+
+        ()
+
+
+    ()
