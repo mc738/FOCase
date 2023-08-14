15f2d7c2417f4015228b2aa8fb94e74baed04e8f
Max Clifford <me@mclifford.dev>
2023-08-14 19:01:29 +0100

    Moved Queries to core

--- FOCase/Queries/Common.fs
+++ FOCase/Queries/Common.fs
@@ -3,5 +3,32 @@
 [<AutoOpen>]
 module Common =
     
+    
+    type Query = { Target: QueryTarget }
+
+    and QueryTarget =
+        | Nodes
+        | Connections
+        | Resources
+        | Documents
+
+    and [<RequireQualifiedAccess>] QueryCondition =
+        | HasTag of Tag: string
+        | Label of LabelWeightCondition
+        | Not of Condition: QueryCondition
+        | And of ConditionA: QueryCondition * ConditionB: QueryCondition
+        | Or of ConditionA: QueryCondition * ConditionB: QueryCondition
+        | Any of Conditions: QueryCondition list
+        | All of Conditions: QueryCondition list
+        | None of Conditions: QueryCondition list
+
+    and LabelWeightCondition =
+        | Equals of Value: decimal
+        | NotEqual of Value: decimal
+        | GreaterThan of Value: decimal
+        | GreaterThanOrEqual of Value: decimal
+        | LessThan of Value: decimal
+        | LessThanOrEqual of Value: decimal
+    
     ()
 
